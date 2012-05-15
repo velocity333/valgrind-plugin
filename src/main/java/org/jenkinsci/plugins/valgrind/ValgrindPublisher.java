85259c219078a9ce4a9b2696f683965e7ef37adc
Johannes Ohlemacher <johannes.ohlemacher@googlemail.com>
2012-05-15 21:39:24 +0200

    fixed data bound constructor

--- src/main/java/org/jenkinsci/plugins/valgrind/ValgrindPublisher.java
+++ src/main/java/org/jenkinsci/plugins/valgrind/ValgrindPublisher.java
@@ -32,6 +32,7 @@ import org.jenkinsci.plugins.valgrind.parser.ValgrindParserResult;
 import org.jenkinsci.plugins.valgrind.util.ValgrindEvaluator;
 import org.jenkinsci.plugins.valgrind.util.ValgrindLogger;
 import org.jenkinsci.plugins.valgrind.util.ValgrindSourceFile;
+import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
@@ -41,12 +42,26 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class ValgrindPublisher extends Recorder
 {
-	private ValgrindPublisherConfig valgrindPublisherConfig;	
+	private ValgrindPublisherConfig valgrindPublisherConfig;
 
-	public ValgrindPublisher(ValgrindPublisherConfig valgrindPublisherConfig)
+	@DataBoundConstructor
+	public ValgrindPublisher( String pattern, 
+			String failThresholdInvalidReadWrite, 
+			String failThresholdDefinitelyLost, 
+			String failThresholdTotal,
+			String unstableThresholdInvalidReadWrite, 
+			String unstableThresholdDefinitelyLost, 
+			String unstableThresholdTotal)
 	{
-		this.valgrindPublisherConfig = valgrindPublisherConfig;
-	}
+		valgrindPublisherConfig = new ValgrindPublisherConfig(
+				pattern, 
+				failThresholdInvalidReadWrite, 
+				failThresholdDefinitelyLost, 
+				failThresholdTotal,
+				unstableThresholdInvalidReadWrite, 
+				unstableThresholdDefinitelyLost, 
+				unstableThresholdTotal );		
+	}	
 
 	@Override
 	public ValgrindPublisherDescriptor getDescriptor()
@@ -256,7 +271,7 @@ public class ValgrindPublisher extends Recorder
 		{
 			return new ValgrindPublisherConfig();
 		}	
-		
+/*		
         @Override
         public Publisher newInstance(StaplerRequest req, JSONObject formData)
                 throws hudson.model.Descriptor.FormException {     
@@ -267,5 +282,6 @@ public class ValgrindPublisher extends Recorder
             ValgrindPublisherConfig config = req.bindJSON(ValgrindPublisherConfig.class, formData);
             return new ValgrindPublisher(config);
         }
+*/        
 	}
 }
